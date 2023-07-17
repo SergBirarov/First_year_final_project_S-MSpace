44b079e07b41ccc75125c4cd1eff915478f84190
barkan33 <mishakolomyza@gmail.com>
2023-07-17 20:39:07 +0300

    Cart

--- scripts/function.js
+++ scripts/function.js
@@ -14,7 +14,7 @@ var storedUsers = [new User(
   `Hell st`,
   `25`
 )];
-var cart = []
+var cart = new Array();
 // arrays for hardcoded products
 var storedFlights = [
   {
@@ -114,7 +114,9 @@ var storedSuits = [
     price: `1360.90`,
   },
 ];
-
+if (!(localStorage.getItem('cart'))) {
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
 if (!(localStorage.getItem('users'))) {
   localStorage.setItem('users', JSON.stringify(storedUsers))
 }
@@ -566,7 +568,7 @@ function AddSuitsToStore() {
 
 function AddFlightToCart(event) {
   let flightIndex = Number(event.target.dataset.ind);
-  let cart = JSON.parse(localStorage.getItem('cart'))
+  cart = JSON.parse(localStorage.getItem('cart'))
   cart.push(storedFlights[flightIndex])
   localStorage.setItem('cart', JSON.stringify(cart));
   UpDatecart();
