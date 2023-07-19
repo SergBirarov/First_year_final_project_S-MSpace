0e3ac0ac8fc07dfdbab01486bfda1900b68e0128
barkan33 <mishakolomyza@gmail.com>
2023-07-19 18:08:05 +0300

    Update function.js

--- scripts/function.js
+++ scripts/function.js
@@ -788,10 +788,10 @@ export function AddShip() {
       return
     }
     if (arrivalDateValue > departureDateValue) {
-      arrivalDateValue.style.border = "2px solid green";
+      arrivalDate.style.border = "2px solid green";
     }
     else {
-      arrivalDateValue.style.border = "2px solid red";
+      arrivalDate.style.border = "2px solid red";
       return
     }
 
